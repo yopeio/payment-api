ab2614915399af7623de6e9daa30aa71f2c93ed4
Enrico Mariotti <enr_74@yahoo.it>
2015-10-03 21:11:12 +0200

    neo4j integration

--- yope-payment-db-neo4j/src/main/java/io/yope/payment/neo4j/config/YopeNeo4jConfiguration.java
+++ yope-payment-db-neo4j/src/main/java/io/yope/payment/neo4j/config/YopeNeo4jConfiguration.java
@@ -1,9 +1,11 @@
 package io.yope.payment.neo4j.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Relationship;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -25,16 +27,27 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Import(RestDataConfiguration.class)
 public class YopeNeo4jConfiguration extends Neo4jConfiguration {
 
+    @ConfigurationProperties(prefix = "neo4j") @Getter
+    @Setter
+    public class Neo4jSettings {
+        private String neo4jUrl;
+        private String neo4jUsername;
+        private String neo4jPassword;
+    }
+
     public YopeNeo4jConfiguration() {
         setBasePackage("io.yope.payment.neo4j.domain");
     }
 
     @Bean
+    public Neo4jSettings neo4jSettings() {
+        return new Neo4jSettings();
+    }
+
+    @Bean
     public GraphDatabaseService graphDatabaseService(
-            @Value("neo4jUrl") final String url,
-            @Value("neo4jUsername") final String username,
-            @Value("neo4jPassword") final String password) {
-        return new SpringCypherRestGraphDatabase(url, username, password);
+            Neo4jSettings settings) {
+        return new SpringCypherRestGraphDatabase(settings.getNeo4jUrl(), settings.getNeo4jUsername(), settings.getNeo4jPassword());
     }
 
     @Override
--- yope-payment-rest/src/main/java/io/yope/payment/rest/YopePaymentApplication.java
+++ yope-payment-rest/src/main/java/io/yope/payment/rest/YopePaymentApplication.java
@@ -1,16 +1,32 @@
 package io.yope.payment.rest;
 
+import io.yope.payment.blockchain.bitcoinj.BitcoinjConfiguration;
+import io.yope.payment.blockchain.bitcoinj.BitcoinjMainNetConfiguration;
+import io.yope.payment.blockchain.bitcoinj.BitcoinjTestNetConfiguration;
+import io.yope.payment.mock.MockConfiguration;
+import io.yope.payment.neo4j.config.RestDataConfiguration;
+import io.yope.payment.neo4j.config.YopeNeo4jConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan("io.yope.payment.blockchain.bitcoinj,io.yope.payment.mock,io.yope.payment.rest.resources")
+@ComponentScan("io.yope.payment.rest.resources, io.yope.payment.blockchain.bitcoinj, io.yope.payment.mock.services")
+@Import({
+        RestDataConfiguration.class,
+        YopeNeo4jConfiguration.class,
+        BitcoinjConfiguration.class,
+        BitcoinjMainNetConfiguration.class,
+        BitcoinjTestNetConfiguration.class,
+//        MockConfiguration.class
+})
+
 public class YopePaymentApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(YopePaymentApplication.class, args);
