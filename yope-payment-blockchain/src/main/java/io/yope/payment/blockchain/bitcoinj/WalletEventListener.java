60b0040ef1b0c09a33b33bce84d041001515865a
Enrico Mariotti <enr_74@yahoo.it>
2015-10-21 15:30:46 +0200

    removed onCoinsReceived notification

--- yope-payment-blockchain/src/main/java/io/yope/payment/blockchain/bitcoinj/WalletEventListener.java
+++ yope-payment-blockchain/src/main/java/io/yope/payment/blockchain/bitcoinj/WalletEventListener.java
@@ -38,10 +38,41 @@ public class WalletEventListener extends AbstractWalletEventListener {
     private final Wallet centralWallet;
 
     @Override
-    public synchronized void onCoinsReceived(final org.bitcoinj.core.Wallet wallet,
-                                             final org.bitcoinj.core.Transaction tx, final Coin prevBalance,
-                                             final Coin newBalance) {
-        super.onCoinsReceived(wallet, tx, prevBalance, newBalance);
+    public void onTransactionConfidenceChanged(final org.bitcoinj.core.Wallet wallet,
+                                               final org.bitcoinj.core.Transaction tx) {
+
+
+        receiveCoins(wallet,tx);
+
+        final String transactionHash = tx.getHashAsString();
+        final Transaction loaded = transactionService.getByTransactionHash(transactionHash);
+        if (loaded == null) {
+            log.warn("transaction hash {} does not yet exist", transactionHash);
+            return;
+        }
+        log.info("-----> confidence changed on {} {}", loaded.getId(), transactionHash);
+        final TransactionConfidence confidence = tx.getConfidence();
+        log.info("new block depth: {} ", confidence.getDepthInBlocks());
+        if (confidence.getDepthInBlocks() >= settings.getConfirmations()) {
+            if (!Transaction.Status.ACCEPTED.equals(loaded.getStatus())) {
+                log.error("transaction {} has status {}", loaded.getId(), loaded.getStatus());
+                return;
+            }
+            try {
+                transactionService.transition(loaded.getId(), Transaction.Status.COMPLETED);
+            } catch (final ObjectNotFoundException e) {
+                log.error("transaction not found", e);
+            } catch (final InsufficientFundsException e) {
+                log.error("Insufficient money", e);
+            } catch (final IllegalTransactionStateException e) {
+                log.error("Illegal transaction state", e);
+            }
+        }
+    }
+
+    private void receiveCoins(final org.bitcoinj.core.Wallet wallet,
+                                             final org.bitcoinj.core.Transaction tx) {
+//        super.onCoinsReceived(wallet, tx, prevBalance, newBalance);
         final Coin valueSentToMe = tx.getValueSentToMe(wallet);
         final Coin valueSentFromMe = tx.getValueSentFromMe(wallet);
 
@@ -53,14 +84,18 @@ public class WalletEventListener extends AbstractWalletEventListener {
         log.info("******** Coins on central wallet to me {} from me {}",  valueSentToMe, valueSentFromMe);
         final String receiverHash = getReceiverHash(tx);
         final String senderHash = getSenderHash(tx);
+        if (receiverHash == null) {
+            return;
+        }
         try {
+
             final Transaction pending = transactionService.getByReceiverHash(receiverHash);
             if (pending == null) {
-                log.error("******** receiver hash {} does not exist", receiverHash);
+                log.debug("******** receiver hash {} does not exist", receiverHash);
                 return;
             }
             if (!Transaction.Status.PENDING.equals(pending.getStatus())) {
-                log.error("******** transaction {} with {} has status {}", pending.getId(), receiverHash, pending.getStatus());
+                log.debug("******** transaction {} with {} has status {}", pending.getId(), receiverHash, pending.getStatus());
                 return;
             }
             final BigDecimal balance = new BigDecimal(valueSentToMe.subtract(valueSentFromMe).longValue()).divide(new BigDecimal(Constants.MILLI_TO_SATOSHI));
@@ -107,35 +142,6 @@ public class WalletEventListener extends AbstractWalletEventListener {
             walletService.save(WalletTO.from(centralWallet).content(DatatypeConverter.printBase64Binary(outputStream.toByteArray())).build());
         } catch (final Exception e) {
             log.error("error adding wallet {}", e);
-        }
-    }
-
-    @Override
-    public void onTransactionConfidenceChanged(final org.bitcoinj.core.Wallet wallet,
-                                               final org.bitcoinj.core.Transaction tx) {
-        final String transactionHash = tx.getHashAsString();
-        final Transaction loaded = transactionService.getByTransactionHash(transactionHash);
-        if (loaded == null) {
-            log.error("transaction hash {} does not exist", transactionHash);
-            return;
-        }
-        log.info("-----> confidence changed on {} {}", loaded.getId(), transactionHash);
-        final TransactionConfidence confidence = tx.getConfidence();
-        log.info("new block depth: {} ", confidence.getDepthInBlocks());
-        if (confidence.getDepthInBlocks() >= settings.getConfirmations()) {
-            if (!Transaction.Status.ACCEPTED.equals(loaded.getStatus())) {
-                log.error("transaction {} has status {}", loaded.getId(), loaded.getStatus());
-                return;
-            }
-            try {
-                transactionService.transition(loaded.getId(), Transaction.Status.COMPLETED);
-            } catch (final ObjectNotFoundException e) {
-                log.error("transaction not found", e);
-            } catch (final InsufficientFundsException e) {
-                log.error("Insufficient money", e);
-            } catch (final IllegalTransactionStateException e) {
-                log.error("Illegal transaction state", e);
-            }
         }
     }
 
