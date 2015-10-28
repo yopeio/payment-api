e4eeb1a01a0475a7196c8d9d2606105d6d592d0c
Gianluigi Davassi <gianluigi.davassi@number26.de>
2015-10-28 22:48:25 +0100

    fixing integration test

--- yope-payment-rest/src/test/java/io/yope/payment/rest/resources/AccountResourceIntegrationTest.java
+++ yope-payment-rest/src/test/java/io/yope/payment/rest/resources/AccountResourceIntegrationTest.java
@@ -1,41 +1,66 @@
 package io.yope.payment.rest.resources;
 
-import com.jayway.restassured.RestAssured;
-import io.yope.payment.rest.helpers.IntegrationTestHelper;
+import static com.jayway.restassured.RestAssured.given;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.util.Assert;
 
-import static com.jayway.restassured.RestAssured.get;
+import com.jayway.restassured.RestAssured;
 
+import io.yope.payment.domain.Account.Type;
+import io.yope.payment.requests.RegistrationRequest;
+import io.yope.payment.rest.helpers.IntegrationTestHelper;
+import io.yope.utils.Serializer;
+
+@lombok.extern.slf4j.Slf4j
 public class AccountResourceIntegrationTest {
 
 	{
 		RestAssured.baseURI = IntegrationTestHelper.LOCALHOST_GOLDRAKE;
 	}
 
-    /**
-     * Ignored because it fails.
-     */
-    @Ignore
 	@Test
-	public void shouldGetVersionIntegrationTest() {
-		get("/health").then().log().all()
-			.assertThat()
-			.statusCode(200);
-			//.body(containsString("v01"));
+	public void shouldCreateAnAccountAngGetAnAccessTokenIntegrationTest() {
+	    final RegistrationRequest registrationRequest = newRegistrationRequest();
+	    
+	    given()
+            .contentType("application/json")
+            .body(Serializer.json(registrationRequest))
+            .post("/accounts").then()
+            .log().all()
+            .assertThat()
+            .statusCode(201);
+	    
+	    log.debug("Registered: {}", registrationRequest);
+	    
+	    Assert.notNull(IntegrationTestHelper
+	            .requestAccessToken(
+	                    registrationRequest.getEmail(), 
+	                    registrationRequest.getPassword()
+	                    ));
 	}
-    /**
-     * Ignored because it fails.
-     */
-    @Ignore
-	@Test
+
+    private RegistrationRequest newRegistrationRequest() {
+        return RegistrationRequest.builder()
+                .email(RandomStringUtils.randomAlphabetic(8) + "@yope.io")
+                .password(RandomStringUtils.randomAlphabetic(8))
+                .firstName(RandomStringUtils.randomAlphabetic(8))
+                .lastName(RandomStringUtils.randomAlphabetic(8))
+                .name(RandomStringUtils.randomAlphabetic(8))
+                .type(Type.SELLER)
+                .build();
+    }
+
+    @Test @Ignore
 	public void shouldOnlyCheckOAuthTest() {
+	    System.err.println(requestAccessToken());
 		Assert.notNull(requestAccessToken());
 	}
 
 	static String requestAccessToken() {
-		return IntegrationTestHelper.requestAccessToken("admin@yope.io", "Vit9uZ2S");
+		return IntegrationTestHelper.requestAccessToken("admisssn@yope.io", "Vit9uZ2S");
 	}
 
 }
