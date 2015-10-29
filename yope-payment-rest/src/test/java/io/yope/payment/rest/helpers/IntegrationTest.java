72e5cf2fdde5ea4554dcf582c7993bde0aef4c0a
Gianluigi Davassi <gianluigi.davassi@number26.de>
2015-10-29 09:32:43 +0100

    integration test exclusion

---
+++ yope-payment-rest/src/test/java/io/yope/payment/rest/helpers/IntegrationTest.java
@@ -0,0 +1,2 @@
+package io.yope.payment.rest.helpers;
+public interface IntegrationTest {}
--- yope-payment-rest/src/test/java/io/yope/payment/rest/resources/AccountResourceIntegrationTest.java
+++ yope-payment-rest/src/test/java/io/yope/payment/rest/resources/AccountResourceIntegrationTest.java
@@ -1,13 +1,16 @@
 package io.yope.payment.rest.resources;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.springframework.util.Assert;
 
 import com.jayway.restassured.RestAssured;
 
+import io.yope.payment.rest.helpers.IntegrationTest;
 import io.yope.payment.rest.helpers.IntegrationTestHelper;
 
 @lombok.extern.slf4j.Slf4j
+@Category(IntegrationTest.class)
 public class AccountResourceIntegrationTest {
 
 	{
@@ -18,6 +21,15 @@ public class AccountResourceIntegrationTest {
 	public void shouldCreateAnAccountAngGetAnAccessTokenIntegrationTest() {
 	    log.info("POST /account test");
 	    Assert.notNull(IntegrationTestHelper.requestAccountAndAccessToken());
+	}
+	
+	@Test
+	public void accessTokenMustBeDifferentForDifferentUsers() {
+	    String accessToken = IntegrationTestHelper.requestAccountAndAccessToken();
+        String accessToken2 = IntegrationTestHelper.requestAccountAndAccessToken();
+        
+        System.err.println(accessToken);
+        System.err.println(accessToken2);
 	}
 
 }
