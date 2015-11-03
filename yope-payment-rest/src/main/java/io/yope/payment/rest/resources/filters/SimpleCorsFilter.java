7ae8d57f265aec1f6f04d691445814b9adccfaa6
mgerardi <massi.gerardi@gmail.com>
2015-11-03 10:56:16 +0100

    added CORS filter

---
+++ yope-payment-rest/src/main/java/io/yope/payment/rest/resources/filters/SimpleCorsFilter.java
@@ -0,0 +1,41 @@
+package io.yope.payment.rest.resources.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(final ServletRequest req, final ServletResponse res,
+            final FilterChain chain) throws IOException, ServletException {
+        final HttpServletResponse response = (HttpServletResponse) res;
+        final HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, access-control-allow-headers, Origin, X-Requested-With, Content-Type, Accept");
+        if (!request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(final FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
